16239ad4f29956ecf6bd3181041ea925f8f35c3e
Quinton Aleong <quinton.aleong@my.uwi.edu>
2022-10-06 21:41:49 -0400

    added initial program

---
+++ lib/NavBar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'download_screen.dart';
+import 'about_screen.dart';
+import 'settings_screen.dart';
+import 'help_screen.dart';
+
+class NavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        // Remove padding
+        padding: EdgeInsets.zero,
+        children: [
+          ListTile(
+            //leading: Icon(Icons.favorite),
+            //title: Text(''),
+            tileColor: Colors.blue,
+            onTap: () => null,
+          ),
+          ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+          ListTile(
+              leading: const Icon(Icons.download),
+              title: const Text('Download'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DownloadScreen()),
+                );
+              }),
+          ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AboutScreen()),
+                    )
+                  }),
+          ListTile(
+              leading: Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SettingsScreen()),
+                );
+              }),
+          ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text('Help'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HelpScreen()),
+                );
+              }),
+          const Divider(),
+          ListTile(
+            title: const Text('Exit'),
+            leading: const Icon(Icons.exit_to_app),
+            onTap: () => null,
+          ),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/download_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DownloadScreen extends StatelessWidget {
+  const DownloadScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Download Screen'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            // Within the SecondScreen widget
+
+            onPressed: () {
+              // Navigate back to the first screen by popping the current route
+              // off the stack.
+
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+}
