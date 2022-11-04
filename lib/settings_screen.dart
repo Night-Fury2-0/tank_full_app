1348a8fd67098dbe860793e2c130c4fee4680155
Nathan Ragoobar <nathan.ragoobar@my.uwi.edu>
2022-11-04 02:41:26 -0400

    Included toggle switch for setting to turn Notifications on and off

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'NavBar.dart';
 import 'globals.dart' as globals;
-
+import 'package:flutter_switch/flutter_switch.dart';
 import 'help_screen.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,17 +24,24 @@ class SettingsScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
 
-            ElevatedButton(
-              onPressed: () {
-                globals.NotificationState = false;
-              },
+          FlutterSwitch(
+            width: 125.0,
+            height: 55.0,
+            valueFontSize: 25.0,
+            toggleSize: 45.0,
+            value: globals.NotificationState,
+            borderRadius: 30.0,
+            padding: 8.0,
+            showOnOff: true,
+            onToggle: (val) {
+              setState(() {
+                globals.NotificationState = val;
+              });
+            },
+          ),
 
-              child: Text('Notification'),
-            ),
-            //
 
-
-        ],
+],
       ),
     );
   }
