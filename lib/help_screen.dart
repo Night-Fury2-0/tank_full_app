633577602c4c39e3671ef605ee83ff602d183b6b
Matthias E <matthias.elliot@my.uwi.edu>
2022-11-04 03:44:43 -0400

    Functioning carousel for hepl page with test images

--- lib/help_screen.dart
+++ lib/help_screen.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'NavBar.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_multi_carousel/carousel.dart';
+import 'package:getwidget/getwidget.dart';
 
-class HelpScreen extends StatelessWidget {
+class HelpScreen extends StatefulWidget {
   const HelpScreen({super.key});
 
+  static List<String> imageList = [
+    'assets/images/test1.jpg',
+    'assets/images/test2.jpg',
+    'assets/images/test3.jpg',
+  ];
+
+  @override
+  State<HelpScreen> createState() => _HelpScreenState();
+}
+
+class _HelpScreenState extends State<HelpScreen> {
   @override
   Widget build(BuildContext context) {
-    final double height = (MediaQuery.of(context).size.height) - 100;
+    final double height = (MediaQuery.of(context).size.height) - 25;
 
     return Scaffold(
       //drawer: NavBar(),
@@ -16,25 +27,29 @@ class HelpScreen extends StatelessWidget {
         title: const Text('Help'),
       ),
       body: Center(
-        child: Carousel(
-            height: height,
-            width: 350,
-            initialPage: 3,
-            allowWrap: false,
-            type: Types.simple,
-            onCarouselTap: (i) {
-              print("onTap $i");
+        child: GFCarousel(
+          height: height,
+          hasPagination: true,
+          passiveIndicator: Color.fromARGB(255, 37, 84, 155),
+          activeIndicator: Color.fromARGB(255, 0, 225, 255),
+          viewportFraction: 1.0,
+          items: HelpScreen.imageList.map(
+            (path) {
+              return Container(
+                margin: EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  //borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  child: Image.asset(path),
+                ),
+              );
             },
-            indicatorType: IndicatorTypes.bubble,
-            arrowColor: Colors.black,
-            axis: Axis.horizontal,
-            showArrow: false,
-            children: List.generate(
-                7,
-                (i) => Center(
-                      child:
-                          Container(color: Colors.red.withOpacity((i + 1) / 7)),
-                    ))),
+          ).toList(),
+          onPageChanged: (index) {
+            setState(() {
+              index;
+            });
+          },
+        ),
       ),
     );
   }
